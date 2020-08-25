48580e82d0742ad9bdc827e2ca5d3efab9eab8b7
Ruben Cancino <luxsoft.cancino@gmail.com>
2020-08-25 13:14:01 -0500

    Fix en user.home access

--- sxcxc-api/src/main/groovy/com/luxsoft/cfdix/v33/CfdiCadenaBuilder33.groovy
+++ sxcxc-api/src/main/groovy/com/luxsoft/cfdix/v33/CfdiCadenaBuilder33.groovy
@@ -58,6 +58,7 @@ class CfdiCadenaBuilder33 {
                 // source = new StreamSource(new File('/Users/rubencancino/dumps/xslt/cadenaoriginal_3_3.xslt'))
                 // source = new StreamSource('http://www.sat.gob.mx/sitio_internet/cfd/3/cadenaoriginal_3_3/cadenaoriginal_3_3.xslt')
             } else {
+              String userHome = System.getProperty('user.home')
               source = new StreamSource(new File("${userHome}/dumps/xslt/cadenaoriginal_3_3.xslt"))
                 // source = new StreamSource(new File('/home/xslt/cadenaoriginal_3_3.xslt'))
             }
