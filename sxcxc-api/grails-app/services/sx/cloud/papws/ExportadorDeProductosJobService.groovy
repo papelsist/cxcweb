e6f50d9e820a38247249f58fc97c3816309f14cc
Ruben Cancino <luxsoft.cancino@gmail.com>
2021-06-23 08:51:41 -0500

    Exportador de clientes

---
+++ sxcxc-api/grails-app/services/sx/cloud/papws/ExportadorDeProductosJobService.groovy
@@ -0,0 +1,46 @@
+package sx.cloud.papws
+
+import javax.annotation.PostConstruct
+
+import groovy.util.logging.Slf4j
+import groovy.transform.TypeCheckingMode
+import groovy.transform.CompileStatic
+
+import org.springframework.scheduling.annotation.Scheduled
+
+import sx.cloud.PapwsSolicitudesService
+
+@Slf4j
+@CompileStatic
+class ExportadorDeProductosJobService  {
+
+
+  static lazyInit = false
+
+  ExportadorDeProductosService exportadorDeProductosService
+
+  @PostConstruct
+  def start() {
+    log.debug('Inicializando exportador de productos ')
+  }
+
+  // @Scheduled(fixedDelay = 10000L)
+  /**
+    * cronExpression: "s m h D M W Y"
+    *                  | | | | | | `- Year [optional]
+    *                  | | | | | `- Day of Week, 1-7 or SUN-SAT, ?
+    *                  | | | | `- Month, 1-12 or JAN-DEC
+    *                  | | | `- Day of Month, 1-31, ?
+    *                  | | `- Hour, 0-23
+    *                  | `- Minute, 0-59
+    *                  `- Second, 0-59
+    */
+  @Scheduled(cron = "0 30 9-19 ? * MON-SAT")
+  void exportarSolicitudes() {
+    // */5 9-18 * * 1-6
+    Date start = new Date()
+    log.info('Exportarando productos JSON file a Firestorage  at:{}', start)
+    exportadorDeProductosService.exportarAsJsonFile()
+
+  }
+}
---
+++ sxcxc-api/grails-app/services/sx/cloud/papws/ExportadorDeProductosService.groovy
@@ -0,0 +1,87 @@
+package sx.cloud.papws
+
+import groovy.json.JsonBuilder
+import sx.core.Venta
+
+import java.text.SimpleDateFormat
+
+import groovy.util.logging.Slf4j
+
+import grails.compiler.GrailsCompileStatic
+import grails.gorm.transactions.Transactional
+
+import com.google.api.core.ApiFuture
+import com.google.cloud.firestore.*
+import com.google.cloud.firestore.SetOptions
+import static com.google.cloud.firestore.DocumentChange.Type.*
+
+import org.apache.commons.lang3.exception.ExceptionUtils
+
+import sx.cloud.PapelsaCloudService
+import sx.core.Producto
+import sx.cloud.LxProducto
+
+
+@Slf4j
+// @GrailsCompileStatic
+// @Transactional
+class ExportadorDeProductosService {
+
+  PapelsaCloudService papelsaCloudService
+
+  @Transactional
+  Date pushToFirestore(Producto producto) {
+    log.debug('Pushing producto: {} en Firebase', producto.clave)
+    LxProducto xp = new LxProducto(producto)
+    ApiFuture<WriteResult> result = papelsaCloudService
+      .getFirestore()
+      .collection('productos')
+      .document(xp.id)
+      .set(xp.toMap(), SetOptions.merge())
+    def updateTime = result.get().getUpdateTime().toDate()
+    log.debug("Producto {} saved in firestore succesful at time : {} " , xp.clave, updateTime)
+    return updateTime
+  }
+
+  @Transactional(readOnly = true)
+  def exportarToFirestore() {
+    Producto.list().each { p ->
+      pushToFirestore(p)
+    }
+  }
+
+  @Transactional(readOnly = true)
+  def exportarAsJsonFile() {
+
+    List<Producto> productos = Producto.list()
+    log.debug('All productos: {}', productos.size())
+    byte[] data = buildJsonFile(productos)
+    Map<String,Object> metaData = [
+      size: data.length
+    ]
+    String documentName="catalogos/productos-all.json"
+    this.papelsaCloudService.uploadDocument(documentName, data, 'application/json', metaData)
+  }
+
+  String buildJsonFile(List<Producto> productos) {
+    JsonBuilder jsonBuilder = new JsonBuilder()
+
+    jsonBuilder(productos) { Producto p ->
+    	id p.id
+      clave p.clave
+      descripcion p.descripcion
+      unidad p.unidad
+      linea p.linea ? p.linea.linea : 'NO_DEFINIDA'
+      marca p.marca ? p.marca.marca : 'NO_DEFINIDA'
+      clase p.clase ? p.clase.clase : 'NO_DEFINIDA'
+      kilos p.kilos
+      gramos p.gramos
+      presentacion p.presentacion
+      precioCredito p.precioCredito
+      precioContado p.precioContado
+	  }
+
+    return jsonBuilder.toString()
+  }
+
+}
